fb8a86039b5a07a4845bc143714c21a8d9f7e3ac
Cozdig <tank-20@list.ru>
2025-10-14 16:37:39 +0300

    HW2 Ready

---
+++ catalog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from catalog.apps import CatalogConfig
+from catalog.views import home_page, contacts_page
+
+app_name = CatalogConfig.name
+
+urlpatterns = [
+    path('', home_page, name="home"),
+    path('contacts/', contacts_page, name="contacts")
+]
