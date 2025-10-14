88c14dbac9c77b9d6416584e1710ec09c3a475f1
Cozdig <tank-20@list.ru>
2025-10-14 16:39:28 +0300

    HW2 fix

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,6 +5,6 @@ from catalog.views import home_page, contacts_page
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home_page, name="home"),
+    path('/', home_page, name="home"),
     path('contacts/', contacts_page, name="contacts")
 ]
