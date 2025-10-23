0b306f40e31741f9b63c8e27d010d61c18270773
Cozdig <tank-20@list.ru>
2025-10-23 17:16:06 +0300

    HW-3 ready

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название")
+    description = models.TextField(verbose_name="Описание")
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Название")
+    description = models.TextField(verbose_name="Описание")
+    image = models.ImageField(upload_to='images/', verbose_name="Изображение")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
+    price = models.IntegerField(verbose_name="Цена")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
