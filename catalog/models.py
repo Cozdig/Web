146fdd3503b5ec212a1e455cd4fcc4000ec7f3bb
Cozdig <tank-20@list.ru>
2025-10-24 16:49:49 +0300

    fix mistakes

--- catalog/models.py
+++ catalog/models.py
@@ -9,6 +9,11 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ['name', ]
+
 class Product(models.Model):
     name = models.CharField(max_length=150, verbose_name="Название")
     description = models.TextField(verbose_name="Описание")
@@ -17,4 +22,10 @@ class Product(models.Model):
     price = models.IntegerField(verbose_name="Цена")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ['name', ]
+
 
