897f7f1953cadfa174a46d79c8cd6b69e0a33edd
Berry_Hunter <tank-20@list.ru>
2025-11-07 19:06:11 +0300

    hw ready

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home_page, contacts_page
+from catalog.views import home_page, contacts_page, product_info_page
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', home_page, name="home"),
-    path('contacts/', contacts_page, name="contacts")
+    path('contacts/', contacts_page, name="contacts"),
+    path('product_info/<int:product_id>/', product_info_page, name="product_info"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
+
 # Create your views here.
 def home_page(request):
-    return render(request, 'home.html')
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+    return render(request, 'home.html', context=context)
 
 def contacts_page(request):
     return render(request, 'contacts.html')
+
+def product_info_page(request, product_id):
+    product = Product.objects.get(pk=product_id)
+    context = {
+        'name' : product.name,
+        'description' : product.description,
+        'image' : product.image,
+        'category': product.category,
+        'price' : product.price,
+        'create_date': product.created_at,
+        'update_date': product.updated_at
+    }
+    return render(request, 'product_info.html', context=context)
